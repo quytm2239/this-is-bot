100b0340633ebc7954654679334dd385b2018089
quytm2239 <quytm2239@gmail.com>
2017-09-13 17:08:41 +0700

    Update logic

--- view/add-data/js/add-data.js
+++ view/add-data/js/add-data.js
@@ -6,5 +6,12 @@ $(document).ready(function(){
 
     btnSubmit.on('click',function () {
         console.log(inputQ.val() + inputG.val() + inputA.val());
+        $.post("/api/add-usual", {
+            q: inputQ.val(),
+            g: inputG.val(),
+            a: inputA.val()
+        }, function(result){
+            console.log(result);
+        });
     });
 });
